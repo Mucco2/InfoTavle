4118b4f279c83e689499d0588807b1f8bb6960a0
Mucco2 <Thebabo8903@gmail.com>
2025-06-17 09:16:00 +0200

    comit

---
+++ assets/js/innfotavle.js
@@ -0,0 +1,13 @@
+// Erstat med IP-adressen på din Windows Server VM
+const serverIpAddress = '10.0.0.1.243'; 
+const apiUrl = `http://${serverIpAddress}:3000/api/data`;
+
+fetch(apiUrl)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data); // Data fra din MS SQL database!
+    // ... din kode til at vise data ...
+  })
+  .catch(error => {
+    console.error('Fejl ved fetch:', error);
+  });
